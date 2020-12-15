83cadb035ab83ca7d51f051e28b2a323077933c1
Matteo <matteo.deleonardis2@gmail.com>
2020-12-15 22:58:18 +0100

    learning on test completed

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
-using UAF2
+ENV["PHAGEDATAPATH"]="/home/matteo/poliTo/Tesi/Data"
+
+using UAF2, PhageFields, PhageData, NLopt
 using Test
 
-@testset "UAF2.jl" begin
-    # Write your tests here.
-end
+boyer = import_dataset(:boyer)
+
+boyer = subdata(boyer, not_disappearing(boyer))
+boyer = add_pseudocounts(boyer, 0.5)
+
+epistasis = EpistasisMu{boyer.A, boyer.L}()
+model = Model(epistasis, boyer.T)
+
+ntrain = round(Int, boyer.S*0.8)
+train, test = randtrain(boyer, ntrain)
+
+starting_point!(model, train)
+prior = set_prior(100.0, model)
+opt = set_optimizer(:LD_MMA, model, train; ftol_rel = 1e-7, Δλ = 1e-10)
+
+get_lambda(m ,p ,d) = m.λ
+get_mu(m, p, d) = [m.x[end]];
+
+model, ret, logP, properties = learn!(model, prior, opt, train, monitor = [get_lambda, get_mu])
