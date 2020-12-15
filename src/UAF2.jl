076976588e17b124e037aa35e0732ea42a34a14e
Matteo <matteo.deleonardis2@gmail.com>
2020-12-15 22:57:41 +0100

    added log_approx, setters and preprocessing

--- src/UAF2.jl
+++ src/UAF2.jl
@@ -2,10 +2,13 @@ module UAF2
 
 using NLopt, PhageData, PhageFields, Statistics, Random, StatsBase
 
+include("log_approx.jl")
 include("import_datasets.jl")
 include("model.jl")
 include("single_seq.jl")
 include("likelihood.jl")
+include("preprocess_utils.jl")
+include("setters.jl")
 include("learn.jl")
 
 end
