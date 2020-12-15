4fd20469f1bc7f1ff298e7c8c27ee4f86e787f70
Matteo <matteo.deleonardis2@gmail.com>
2020-12-15 21:57:54 +0100

    learn added

--- src/UAF2.jl
+++ src/UAF2.jl
@@ -2,6 +2,10 @@ module UAF2
 
 using NLopt, PhageData, PhageFields, Statistics, Random, StatsBase
 
-# Write your package code here.
+include("import_datasets.jl")
+include("model.jl")
+include("single_seq.jl")
+include("likelihood.jl")
+include("learn.jl")
 
 end
