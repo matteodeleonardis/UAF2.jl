2fd7f8f9605be5c80298d5382ccf7ac588dd6c8a
Matteo <matteo.deleonardis2@gmail.com>
2020-12-16 21:02:04 +0100

    added rare binding learning and using PyPlot

--- src/UAF2.jl
+++ src/UAF2.jl
@@ -1,6 +1,6 @@
 module UAF2
 
-using NLopt, PhageData, PhageFields, Statistics, Random, StatsBase
+using NLopt, PhageData, PhageFields, Statistics, Random, StatsBase, PyPlot
 
 include("log_approx.jl")
 include("import_datasets.jl")
@@ -10,5 +10,12 @@ include("likelihood.jl")
 include("preprocess_utils.jl")
 include("setters.jl")
 include("learn.jl")
+include("validation_utils.jl")
+include("likelihood_rare.jl")
+include("learn_rare.jl")
+
+    module BioseqUtils_dev
+        include("BioseqUtils_dev.jl")
+    end
 
 end
